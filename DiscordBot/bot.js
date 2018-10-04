049e504a48b8a22bd01a067bdad1e243b3331a57
BonnW <pwonghansa@gmail.com>
2018-10-04 02:40:40 -0500

    added line spacing for more readability and changed vars to consts/lets

--- DiscordBot/bot.js
+++ DiscordBot/bot.js
@@ -1,14 +1,20 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
+
 // Configure logger settings
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
+
+const bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
@@ -21,8 +27,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+        let args = message.substring(1).split(' ');
+        let cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
